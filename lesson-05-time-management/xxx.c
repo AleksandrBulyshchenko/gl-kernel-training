647f16a58f16e4a1049e7d9eec2b8ec349702065
ASP <saxel@ya.ru>
2017-10-25 00:40:51 +0300

    Fibonacci problem has been solved

--- lesson-05-time-management/xxx.c
+++ lesson-05-time-management/xxx.c
@@ -5,14 +5,48 @@
 #include <linux/pci.h>
 #include <linux/version.h>
 #include <linux/init.h>
+#include <linux/timer.h>
+#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <asm-generic/param.h>
 
 #define LEN_MSG 160
 static char buf_msg[ LEN_MSG + 1 ] = "Hello from module!\n";
+static long long ll_current_fib_num = 0;
+static struct timer_list one_sec_timer;
 
+static long long x_get_fibo(void)
+{
+   static long long fib_lo = 1;
+   static long long fib_hi = 2;
+   long long tmp = fib_lo;
+   fib_lo = fib_hi;
+   fib_hi += tmp;
+   return fib_hi;
+}
+
+static long long get_current_fib_num(void)
+{
+   //TODO Add spin_lock()
+   long long tmp = ll_current_fib_num;
+   return tmp;
+}
+
+static void update_current_fib_num(unsigned long data)
+{
+   //TODO Add spin_lock()
+   ll_current_fib_num = x_get_fibo();
+   printk("Timer tick");
+   one_sec_timer.expires = get_jiffies_64() + 1*HZ;
+   add_timer(&one_sec_timer);
+}
 
 static ssize_t xxx_show( struct class *class, struct class_attribute *attr, char *buf ) {
+   long long num = get_current_fib_num();
+   memset(buf_msg, 0, LEN_MSG);
+   sprintf(buf_msg, "current fibonacci number is %lld\n", num);
    strcpy( buf, buf_msg );
-   printk( "read %ld\n", (long)strlen( buf ) );
+   printk( "%s", buf_msg );
    return strlen( buf );
 }
 
@@ -29,14 +63,22 @@ static struct class *x_class;
 
 int __init x_init(void) {
    int res;
+   u64 jiff = 0;
    x_class = class_create( THIS_MODULE, "x-class" );
    if( IS_ERR( x_class ) ) printk( "bad class create\n" );
    res = class_create_file( x_class, &class_attr_xxx );
    printk( "'xxx' module initialized\n" );
+   init_timer(&one_sec_timer);
+   jiff = get_jiffies_64();
+   one_sec_timer.expires = jiff + 1*HZ;
+   one_sec_timer.data = 0;
+   one_sec_timer.function = update_current_fib_num ;
+   add_timer(&one_sec_timer);
    return res;
 }
 
 void x_cleanup(void) {
+   del_timer(&one_sec_timer);
    class_remove_file( x_class, &class_attr_xxx );
    class_destroy( x_class );
    return;
