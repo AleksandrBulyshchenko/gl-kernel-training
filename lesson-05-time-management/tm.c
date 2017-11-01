801e68d00506a6d4ba2312c0b8b651dfc6600240
oleksii-kogutenko <oleksii.kogutenko@gmail.com>
2017-11-01 11:08:48 +0200

    Added timer to calculate fibo value
     * Add fibo_timer file as "/sys/class/tm/fibo_timer" for Show fibo value
       from that increasing in the timer irq

--- lesson-05-time-management/tm.c
+++ lesson-05-time-management/tm.c
@@ -7,27 +7,42 @@
 #include <linux/init.h>
 #include <linux/sched.h>
 #include <linux/time.h>
-
+#include <linux/spinlock.h>
+//
+// https://kernel.readthedocs.io/en/sphinx-samples/kernel-locking.html
+// Documentation/locking/spinlocks.txt
+//
 MODULE_LICENSE( "GPL" );
 MODULE_AUTHOR("Oleksii Kogutenko");
 MODULE_VERSION("1.0");
 MODULE_DESCRIPTION("Kernel training: 05 - Internal Kernel API (Time Management)");
 
-static u64 last_sys_time = 0;
-static struct timeval last_abs_time = { 0, 0 };
+static u64              last_sys_time = 0;          //jiff counter
+static struct timeval   last_abs_time = { 0, 0 };   //abs time
 
 #define FIBO_ARRAY_SIZE 3
 struct t_fibo_struct {
-    u64 data[FIBO_ARRAY_SIZE];
-    int cur, iteration;
+    u64                 data[FIBO_ARRAY_SIZE];  // Data used for fibo calculate
+    int                 cur;                    // Current data pointer
+    int                 iteration;              // Number of current iteration of fibo calculation
+    struct timer_list   timer;                  // timer structure
+    spinlock_t          lock;                   // Spin lock to use in timer
 };
+#define FIBO_STRUCT_INIT {\
+    .data = {0,1,1}, \
+    .cur = 0, \
+    .iteration = 0 \
+}
 
-static struct t_fibo_struct fibo_next = {
-    .data = {0,1,1},
-    .cur = 0,
-    .iteration = 0
-};
+static struct t_fibo_struct fibo_next  = FIBO_STRUCT_INIT;
+static struct t_fibo_struct fibo_timer = FIBO_STRUCT_INIT;
 
+#define TIMER_FIBO_PERIOD   (1 * HZ) /* 1 sec */
+
+// ----------------------------------------------------------------------------------------
+// Show jiff time (in seconds) passed since previous read of it.
+// /sys/class/tm/jiff
+// ----------------------------------------------------------------------------------------
 static ssize_t tm_jiff_show( struct class *class, struct class_attribute *attr, char *buf ) 
 {
     size_t count = 0;
@@ -50,8 +65,12 @@ static ssize_t tm_jiff_show( struct class *class, struct class_attribute *attr, 
 
     return count;
 }
- 
-static ssize_t tm_abs_show( struct class *class, struct class_attribute *attr, char *buf ) {
+// ----------------------------------------------------------------------------------------
+// Show absolute time of previous reading
+// /sys/class/tm/abs
+// ----------------------------------------------------------------------------------------
+static ssize_t tm_abs_show( struct class *class, struct class_attribute *attr, char *buf ) 
+{
     size_t count = 0;
 
     struct timeval tv = { 0, 0 };
@@ -77,19 +96,45 @@ static ssize_t tm_abs_show( struct class *class, struct class_attribute *attr, c
 
     return count;
 }
-
-static ssize_t tm_fibo_show( struct class *class, struct class_attribute *attr, char *buf ) {
-    size_t count = 0;
-
-    int fibo_i1 = (fibo_next.cur > 0) ? (fibo_next.cur - 1) : FIBO_ARRAY_SIZE - 1;
+// ----------------------------------------------------------------------------------------
+// Update fibo structure
+// Unsafe function
+// ----------------------------------------------------------------------------------------
+static u64 update_fibo(struct t_fibo_struct *fibo)
+{
+    int fibo_i1 = (fibo->cur > 0) ? (fibo->cur - 1) : FIBO_ARRAY_SIZE - 1;
     int fibo_i2 = (fibo_i1 > 0) ? (fibo_i1 - 1) : FIBO_ARRAY_SIZE - 1;
-    u64 fibo_val = (fibo_next.iteration < FIBO_ARRAY_SIZE) ? fibo_next.data[fibo_next.iteration] : fibo_next.data[fibo_i1] + fibo_next.data[fibo_i2];
+    u64 fibo_val = (fibo->iteration < FIBO_ARRAY_SIZE) ? fibo->data[fibo->iteration] : fibo->data[fibo_i1] + fibo->data[fibo_i2];
     
-    if (fibo_next.iteration >= FIBO_ARRAY_SIZE) fibo_next.data[fibo_next.cur] = fibo_val;
+    if (fibo->iteration >= FIBO_ARRAY_SIZE) fibo->data[fibo->cur] = fibo_val;
     
-    fibo_next.cur = (fibo_next.cur + 1) % FIBO_ARRAY_SIZE;
-    fibo_next.iteration++;
+    fibo->cur = (fibo->cur + 1) % FIBO_ARRAY_SIZE;
+    fibo->iteration++;
 
+    return fibo_val;
+}
+// ----------------------------------------------------------------------------------------
+// Get current fibo value
+// Unsafe function
+// ----------------------------------------------------------------------------------------
+static u64 get_fibo_current_value(struct t_fibo_struct *fibo)
+{
+    int fibo_i1 = (fibo->cur > 0) ? (fibo->cur - 1) : FIBO_ARRAY_SIZE - 1;
+    u64 fibo_val = (fibo->iteration < FIBO_ARRAY_SIZE) ? fibo->data[fibo->iteration] : fibo->data[fibo_i1];
+
+    return fibo_val;
+}
+// ----------------------------------------------------------------------------------------
+// Show next fibo value since previous read of it
+// /sys/class/tm/fibo
+// ----------------------------------------------------------------------------------------
+static ssize_t tm_fibo_show( struct class *class, struct class_attribute *attr, char *buf ) 
+{
+    size_t count = 0;
+    u64 fibo_val;
+
+    fibo_val = update_fibo(&fibo_next);
+    
     sprintf( buf, "Fibo value is %llu {cur %d, {%llu, %llu, %llu}}\n", fibo_val, fibo_next.cur, fibo_next.data[0], fibo_next.data[1], fibo_next.data[2] );
 
     count = strlen( buf );
@@ -97,8 +142,71 @@ static ssize_t tm_fibo_show( struct class *class, struct class_attribute *attr, 
 
     return count;
 }
+// ----------------------------------------------------------------------------------------
+// Show fibo value from that increasing in the timer irq
+// /sys/class/tm/fibo_timer
+// ----------------------------------------------------------------------------------------
+static ssize_t tm_fibo_timer_show( struct class *class, struct class_attribute *attr, char *buf ) 
+{
+    size_t count = 0;
+    unsigned long flags;
+    u64 fibo_val;
+    u64 iterations;
 
+    spin_lock_irqsave(&fibo_timer.lock, flags);
+    
+    fibo_val = get_fibo_current_value(&fibo_timer);
+    iterations = fibo_timer.iteration;
+    
+    spin_unlock_irqrestore(&fibo_timer.lock, flags);
 
+    sprintf( buf, "Fibo value is %llu (iterations: %llu)\n", fibo_val, iterations);
+
+    count = strlen( buf );
+    printk( "read %ld\n", (long)count );
+
+    return count;
+}
+// ----------------------------------------------------------------------------------------
+// timer callback
+// ----------------------------------------------------------------------------------------
+void fibo_timer_callback(unsigned long data)
+{
+    struct t_fibo_struct *fibo = (struct t_fibo_struct*) data;
+    unsigned long flags; 
+    u64 jiff, diff;
+
+    spin_lock_irqsave(&fibo->lock, flags);
+
+    update_fibo(&fibo_timer);
+    
+    jiff = get_jiffies_64();
+    diff = (jiff - fibo->timer.expires) + TIMER_FIBO_PERIOD + jiff;
+
+    mod_timer(&fibo->timer, diff);
+
+    spin_unlock_irqrestore(&fibo->lock, flags);
+}
+// ----------------------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------------------
+static int init_fibo_timer(struct t_fibo_struct *fibo)
+{
+    int ret = 0;
+    spin_lock_init(&fibo->lock);
+    init_timer(&fibo->timer);
+    setup_timer(&fibo->timer, fibo_timer_callback, (unsigned long)fibo);
+    ret = mod_timer(&fibo->timer, get_jiffies_64() + TIMER_FIBO_PERIOD);
+    return ret;
+}
+// ----------------------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------------------
+static void deinit_fibo_timer(struct t_fibo_struct *fibo)
+{
+    del_timer(&fibo->timer);
+}
+// ----------------------------------------------------------------------------------------
 #define CLASS__ATTR_RO(_name, _data) {						\
 	.attr	= { .name = __stringify(_data), .mode = S_IRUGO },	\
 	.show	= _name##_##_data##_show,						\
@@ -108,7 +216,7 @@ static ssize_t tm_fibo_show( struct class *class, struct class_attribute *attr, 
     static struct class* name##_class;
 
 #define CLASS_DATA_RO(name, data) \
-    struct class_attribute class_attr_##name##data = CLASS__ATTR_RO( name, data ); \
+    struct class_attribute class_attr_##name##data = CLASS__ATTR_RO( name, data );
 
 #define CREATE_CLASS(name, goto_err) \
     ({ \
@@ -128,16 +236,20 @@ static ssize_t tm_fibo_show( struct class *class, struct class_attribute *attr, 
     })
 
 #define REMOVE_FILE(name, data) \
-    class_remove_file( name##_class, &class_attr_##name##data ); \
+    class_remove_file( name##_class, &class_attr_##name##data );
 
 #define REMOVE_CLASS(name) \
     class_destroy( name##_class );
-
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 CLASS(tm);
 CLASS_DATA_RO(tm, jiff);
 CLASS_DATA_RO(tm, abs);
 CLASS_DATA_RO(tm, fibo);
-
+CLASS_DATA_RO(tm, fibo_timer);
+// ----------------------------------------------------------------------------------------
+// Init module function
+// ----------------------------------------------------------------------------------------
 int __init tm_init(void) {
     int res;
 
@@ -145,9 +257,16 @@ int __init tm_init(void) {
     res  = CREATE_FILE(tm, jiff, jiff_err);
     res |= CREATE_FILE(tm, abs,  abs_err);
     res |= CREATE_FILE(tm, fibo, fibo_err);
+    res |= CREATE_FILE(tm, fibo_timer, fibo_timer_err);
+    if (init_fibo_timer(&fibo_timer)) goto fibo_timer_init_error;
    
     printk( "'tm' module initialized %d\n", res );
     return res;
+fibo_timer_init_error:
+    printk("fibo timer init error!\n");
+    deinit_fibo_timer(&fibo_timer);
+fibo_timer_err:
+    REMOVE_FILE(tm, fibo_timer);
 fibo_err:
     REMOVE_FILE(tm, fibo);
 abs_err:
@@ -158,8 +277,13 @@ create_class_err:
     REMOVE_CLASS(tm);
     return res;
 }
- 
-void tm_cleanup(void) {
+// ----------------------------------------------------------------------------------------
+// Exit module function
+// ----------------------------------------------------------------------------------------
+void tm_exit(void) {
+    deinit_fibo_timer(&fibo_timer);
+
+    REMOVE_FILE(tm, fibo_timer);
     REMOVE_FILE(tm, fibo);
     REMOVE_FILE(tm, abs);
     REMOVE_FILE(tm, jiff);
@@ -167,8 +291,8 @@ void tm_cleanup(void) {
     REMOVE_CLASS(tm);
    return;
 }
- 
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 module_init( tm_init );
-module_exit( tm_cleanup );
- 
+module_exit( tm_exit );
 
