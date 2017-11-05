aee0bc4397f726a0ed25f496b9db4e6a3059b9a2
srybalko <rybalko.serhii.o@gmail.com>
2017-11-05 14:21:30 +0200

    lesson 5 code refactoring, some issue in makefile

--- lesson-05-time-management/fib.c
+++ lesson-05-time-management/fib.c
@@ -1,102 +1,140 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
 
 #include "fib.h"
 
 
-void fib_get_str(const uint8_t *a, uint8_t *str, uint32_t length)
-{
-    uint16_t i=length-1;
-    //char* sprf_ptr = str;
-    
-    if(length>FIB_BUFFSIZE) return;
-    
-    if(!(*(a+length-1)>>4))
-    {
-        //printk("%u", *(a+length-1));
-        snprintf(str, FIB_BUFFSIZE, "%u", *(a+length-1));
-        str++;
-    }
-    else
-    {
-        snprintf(str, FIB_BUFFSIZE, "%02x", *(a+length-1));
-        str+=2;
-    }
-
-    for( ; i>0; i--)
-    {
-        snprintf(str, FIB_BUFFSIZE, "%02x", *(a+i-1));
-        str+=2;
-    }
-    snprintf(str, FIB_BUFFSIZE, "\n");
-    
-    //printk("%s", out_buf);
-    
-    /*out_buf
-    int snprintf(char *buf, size_t size, const char *fmt, ...);*/
-}
-
-bool sum_all(const uint8_t *a, const uint8_t *b, uint8_t *c, uint32_t length)
+/************************************************************************************************
+ *                  Calculation sum of two binary-coded decimal arrays
+ *
+ *  retval: boolean overflow
+ ***********************************************************************************************/
+bool sum_all(const uint8_t *addend1, const uint8_t *addend2, uint8_t *res, uint32_t length)
 {
     bool overflow = false;
     int j = 0;
     for(;j<length; j++)
     {
-        //*(c+j) = 0;
         int i=0;
         for(;i<2; i++)
         {
-            uint8_t sm = ((*(a+j)>>4*i)&0xf) + ((*(b+j)>>4*i)&0xf) + (overflow == true);
+            // Sum of two binary-coded digits:
+            uint8_t sm = ((*(addend1+j)>>4*i)&0xf) + ((*(addend2+j)>>4*i)&0xf) + ((overflow == true)?1:0);
             
             uint8_t add;
             
-            if(sm>9)
+            if(sm>9) // Need add next order of magnitude
             {
                 add = sm-10;
                 overflow = true;
             }
             else
             {
-                add = sm;
+                add = sm; // Is enough current size of magnitude
                 overflow = false;
             }
+
+            // Clear the binary-coded digit for writing:
+            if(i) *(res+j) &= 0x0F; else *(res+j) &= 0xF0;
             
-            //printf("add: %u %u sm: %u\n", ((*(a+j)>>4*i)&0xf) , ((*(b+j)>>4*i)&0xf),sm);
-            
-            if(i) *(c+j) &= 0x0F; else *(c+j) &= 0xF0;
-            
-            *(c+j) |= (add<<4*i);
+            // Writing the binary-coded digit of sum:
+            *(res+j) |= (add<<4*i);
         }
     }
     return overflow;
 }
 
-bool sum_short(const uint8_t *a, const uint8_t *b, uint8_t *c, uint16_t *length, uint16_t max_len)
+/************************************************************************************************
+ *        Calculation sum of first significant bytes of two binary-coded decimal arrays
+ *           If res buffer has free space his length will increase in overflow case
+ *
+ *  retval: boolean overflow
+ ***********************************************************************************************/
+bool sum_short(const uint8_t *addend1, const uint8_t *addend2, uint8_t *res, uint16_t *length, uint16_t max_len)
 {
-    bool overflow = sum_all(a,b,c,*length);
+    bool overflow = sum_all(addend1, addend2, res, *length);
     
     if(overflow)
     {
         if(*length >= max_len)
-            return true;
+            return true; // Buffer hasn't any free space
         else
         {
-            //printf("len=%u\n", *length);
-            *(c+*length) = 0x01;
+            // Buffer has free space; add one element
+            *(res+*length) = 0x01;
             (*length)++;
-            //printf("len=%u\n", *length);
-            
         }
     }
     return false;
 }
 
-bool fib_step(fb_t *fib)
+/************************************************************************************************
+ *                           PUBLIC FUNCTIONS IMPLEMENTATIONS
+ ***********************************************************************************************/
+
+void fib_init(fb_t *f)
 {
-    memcpy(fib->tmp, fib->fib_buf1, fib->fib_curr_len);
-    fib->ovf = sum_short(fib->fib_buf0,fib->fib_buf1,fib->fib_buf1,&(fib->fib_curr_len),FIB_BUFFSIZE);
-    memcpy(fib->fib_buf0, fib->tmp, fib->fib_curr_len);
-    fib->fib_iter++;
-    return fib->ovf;
+    // Create buffers for fib numbers:
+    f->fib_buf0 = (uint8_t*) kmalloc(FIB_BUFFSIZE, 0);
+    f->fib_buf1 = (uint8_t*) kmalloc(FIB_BUFFSIZE, 0);
+    memset(f->fib_buf0, 0, FIB_BUFFSIZE);
+    memset(f->fib_buf1, 0, FIB_BUFFSIZE);
+
+    // Init first value
+    f->fib_buf1[0]=0x01;
+    f->fib_curr_len = 1;
+    f->fib_iter = 0;
+    f->ovf = false;
 }
+
+void fib_uninit(fb_t *f)
+{
+    kfree(f->fib_buf0);
+    kfree(f->fib_buf1);
+}
+
+bool fib_step(fb_t *f)
+{
+    uint16_t tmpvar_len = f->fib_curr_len;
+    uint8_t* tmpvar = (uint8_t*) kmalloc(tmpvar_len, 0);
+    // Save the second Fibonacci number:
+    memcpy(tmpvar, f->fib_buf1, f->fib_curr_len);
+    // Calculate next Fibonacci number:
+    f->ovf = sum_short(f->fib_buf0,f->fib_buf1,f->fib_buf1,&(f->fib_curr_len),FIB_BUFFSIZE);
+    // Restore to the first Fibonacci number:
+    memcpy(f->fib_buf0, tmpvar, tmpvar_len);
+    kfree(tmpvar);
+    f->fib_iter++;
+
+    return f->ovf;
+}
+
+void fib_get_str(fb_t *f, char *buf)
+{
+    uint16_t i = f->fib_curr_len-1;
+
+    // Copy the highest digit
+    if(!(*(f->fib_buf1+f->fib_curr_len-1)>>4))
+    {
+        // 0X case
+        snprintf(buf, FIB_BUFFSIZE, "%u", *(f->fib_buf1+f->fib_curr_len-1));
+        buf++;
+    }
+    else
+    {
+        // XX case
+        snprintf(buf, FIB_BUFFSIZE, "%02x", *(f->fib_buf1+f->fib_curr_len-1));
+        buf+=2;
+    }
+
+    for( ; i>0; i--)
+    {
+        snprintf(buf, FIB_BUFFSIZE, "%02x", *(f->fib_buf1+i-1));
+        buf+=2;
+    }
+    snprintf(buf, FIB_BUFFSIZE, "\n");
+}
+
+MODULE_LICENSE( "GPL" );
--- lesson-05-time-management/usrmod.c
+++ lesson-05-time-management/usrmod.c
@@ -9,38 +9,30 @@
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
 
+#include "fib.h"
+#warning Problem with building multiple file modules, please see Makefile
 #include "fib.c"
 
-static struct timer_list one_sec_tmr;
-
-static char* out_buf;
-
-fb_t fb_num;
-
 static struct class *f_class;
+static struct timer_list one_sec_tmr;
+static fb_t fb_num;
 
-static uint32_t lst_reading_tmr = 0;
-
-static ssize_t feb_show( struct class *class, struct class_attribute *attr, char *buf ) {
-   fib_get_str(fb_num.fib_buf1, out_buf, fb_num.fib_curr_len);
-   strcpy( buf, out_buf );
-   printk( "read %ld bytes [Fibonacci #%u]\n", (long)strlen( buf ), fb_num.fib_iter );
-   return strlen( buf );
+static ssize_t feb_show( struct class *class, struct class_attribute *attr, char *buf )
+{
+    fib_get_str(&fb_num, buf);
+    printk( "read %ld bytes [Fibonacci #%u]\n", (long)strlen( buf ), fb_num.fib_iter );
+    return strlen( buf );
 }
 
-static ssize_t lst_r_tmr_show( struct class *class, struct class_attribute *attr, char *buf ) {
-    if(!lst_reading_tmr)
-    {
-        const char out[] = "Newer\n";
-        strcpy(buf, out);
-        
-    }
+static ssize_t lst_r_tmr_show( struct class *class, struct class_attribute *attr, char *buf )
+{
+    static uint32_t lst_reading_tmr = 0;
+
+    if(lst_reading_tmr)
+        snprintf(buf, 50, "last reading was %u secs ago\n", jiffies_to_msecs(jiffies)/1000-lst_reading_tmr);
     else
-    {
-        char out[50];
-        snprintf(out, 50, "last reading was %u secs ago\n", jiffies_to_msecs(jiffies)/1000-lst_reading_tmr);
-        strcpy(buf, out);
-    }
+       strcpy(buf, "Newer\n");
+
     lst_reading_tmr = jiffies_to_msecs(jiffies)/1000;
     return strlen( buf );
 }
@@ -52,53 +44,63 @@ CLASS_ATTR_RO( lst_r_tmr );
 void timer_handler(unsigned long data)
 {
     if(!fib_step(&fb_num))
-        mod_timer( &one_sec_tmr, jiffies + msecs_to_jiffies(1000) );
+    {
+        if(mod_timer( &one_sec_tmr, jiffies + msecs_to_jiffies(1000)))
+                printk("usrmod: Timer starting fault/n");
+    }
     else
-        printk("fib_overflow");
+        printk("usrmod: Fibonacci buffer overflow\n");
 }
 
 int __init mod_init(void)
 {
-    int res = 0;
-    
+    printk("usrmod start...\n");
+
     f_class = class_create( THIS_MODULE, "feb-class" );
-    if( IS_ERR( f_class ) ) printk( "bad class create\n" );
-    res |= class_create_file( f_class, &class_attr_feb );
-    res |= class_create_file( f_class, &class_attr_lst_r_tmr );
-    
-    fb_num.fib_buf0 = (uint8_t*) kmalloc(FIB_BUFFSIZE, 0);
-    fb_num.fib_buf1 = (uint8_t*) kmalloc(FIB_BUFFSIZE, 0);
-    memset(fb_num.fib_buf0, 0, FIB_BUFFSIZE);
-    memset(fb_num.fib_buf1, 0, FIB_BUFFSIZE);
-    fb_num.fib_buf1[0]=0x01;
-    fb_num.tmp = (uint8_t*) kmalloc(FIB_BUFFSIZE, 0);
-    memset(fb_num.tmp, 0, FIB_BUFFSIZE);
-    fb_num.fib_curr_len = 1;
-    fb_num.fib_iter = 0;
-    fb_num.ovf = false;
-    out_buf = (char*) kmalloc(FIB_BUFFSIZE*2, 0);
-    memset(out_buf, 0, FIB_BUFFSIZE*2);
-    
+    if( IS_ERR( f_class ) )
+        goto err_cc;
+    printk("usrmod: feb-class created\n");
+
+    if(class_create_file( f_class, &class_attr_feb ))
+        goto err_cf_feb;
+    printk("usrmod: file feb created\n");
+
+
+    if(class_create_file( f_class, &class_attr_lst_r_tmr ))
+        goto err_cf_tmr;
+    printk("usrmod: file lst_r_tmr created\n");
+
+    fib_init(&fb_num);
     
     setup_timer(&one_sec_tmr, timer_handler, 0);
-    
-    printk( "timer start\n" );
-    res |= mod_timer( &one_sec_tmr, jiffies + msecs_to_jiffies(1000) );
-    if (res) printk("Error in mod_timer\n");
-    
-    return res;
+
+    if(mod_timer( &one_sec_tmr, jiffies + msecs_to_jiffies(1000)))
+        goto err_tmr;
+    printk( "usrmod: timer started\n" );
+
+    printk( "usrmod initialized -----------\n" );
+    return 0;
+
+err_tmr:
+    fib_uninit(&fb_num);
+    class_remove_file( f_class, &class_attr_lst_r_tmr );
+err_cf_tmr:
+    class_remove_file( f_class, &class_attr_feb );
+err_cf_feb:
+    class_destroy( f_class );
+err_cc:
+    printk( "usrmod initialization fault\n" );
+    return -1;
 }
 
 void mod_cleanup(void) 
 {
     del_timer( &one_sec_tmr );
-    kfree(fb_num.fib_buf0);
-    kfree(fb_num.fib_buf1);
-    kfree(fb_num.tmp);
-    kfree(out_buf);
+    fib_uninit(&fb_num);
     class_remove_file( f_class, &class_attr_feb );
     class_remove_file( f_class, &class_attr_lst_r_tmr );
     class_destroy( f_class );
+    printk( "usrmod removed ---------------\n" );
     return;
 }
 
