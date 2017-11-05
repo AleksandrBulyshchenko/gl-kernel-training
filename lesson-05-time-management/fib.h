9ed28cd7a7aac00840555ec87901c9150a731d89
srybalko <rybalko.serhii.o@gmail.com>
2017-11-05 14:21:30 +0200

    lesson 5 code refactoring, some issue in makefile

--- lesson-05-time-management/fib.h
+++ lesson-05-time-management/fib.h
@@ -1,17 +1,24 @@
+#ifndef FIB_H
+#define FIB_H
+
 
 #define FIB_BUFFSIZE 1000
 
 typedef struct
 {
-    uint8_t *fib_buf0;
-    uint8_t *fib_buf1;
-    uint8_t *tmp;
-    uint16_t fib_curr_len;
     uint32_t fib_iter;
+    uint16_t fib_curr_len;
+    uint8_t *fib_buf0;
+    uint8_t *fib_buf1;  
     bool ovf;
 }fb_t; 
 
 
-void fib_get_str(const uint8_t *a, uint8_t *str, uint32_t length);
+void fib_init(fb_t *f);
+void fib_uninit(fb_t *f);
+bool fib_step(fb_t *f);
+void fib_get_str(fb_t *f, char *buf);
 
-bool fib_step(fb_t *fib);
+
+
+#endif //FIB_H
